12c79c73bc47d2494933dc7024fad17f21e344c0
Edge <edge27452696803@gmail.com>
2025-07-03 08:40:53 +0800

    任务：移除未使用的图片和脚本，添加 favicon 和 robots.txt 文件
    - 从公共目录中删除了多个未使用的图片文件。
    - 移除了过时的脚本：copy-db.js 和 update-readme-version.js。
    - 添加了新的 favicon.ico 文件。
    - 引入了 robots.txt 文件来管理网络爬虫的访问权限。
    - 更新了服务器和项目的 TypeScript 配置文件。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,117 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { version } from 'os';
-import { transformAssetUrls } from 'vite-plugin-vuetify';
-import tailwindcss from '@tailwindcss/vite'
-
-export default {
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  build: {
-    transpile: ['vuetify']
-  },
-  compatibilityDate: '2024-07-08',
+
   modules: [
-    'vuetify-nuxt-module',
+    '@nuxt/content',
     '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/i18n',
-    '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt',
-    '@element-plus/nuxt',
-    'shadcn-nuxt'
-  ],
-
-  css: ['~/assets/css/tailwind.css'],
-
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls
-      }
-    },
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-  
-  nitro: {
-    storage: {
-      'local': {
-        driver: 'fs',
-        base: './data'
-      }
-    },
-    experimental: {
-      asyncContext: true,
-      database: true
-    }
-  },
-
-  i18n: {
-    vueI18n: './i18n.config.ts', // if you are using custom path, default
-    locales: ['zh', 'en'],
-    strategy: 'prefix_except_default', 
-    defaultLocale: 'zh'
-  },
-
-  runtimeConfig: {
-    jwtSecret: process.env.JWT_SECRET,
-    oracleHost: process.env.ORACLE_HOST,
-    oraclePort: process.env.ORACLE_PORT,
-    oracleSid: process.env.ORACLE_SID,
-    oracleServiceName: process.env.ORACLE_SERVICE_NAME,
-    oracleUser: process.env.ORACLE_USER,
-    oraclePassword: process.env.ORACLE_PASSWORD,
-  },
-
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '',
-    storageKey: 'nuxt-color-mode'
-  },
-
-  vuetify: {
-    moduleOptions: {
-      ssrClientHints: {
-        prefersColorScheme: true
-      }
-    },
-    vuetifyOptions: {
-      icons: {
-        defaultSet: 'mdi'
-      }
-    },
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      themes: {
-        dark: {
-          primary: '#1976D2',
-          secondary: '#424242',
-          accent: '#82B1FF',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FB8C00'
-        },
-        light: {
-          primary: '#1976D2',
-          secondary: '#424242',
-          accent: '#82B1FF',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FB8C00'
-        }
-      }
-    }
-  },
-
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  }
-};
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui'
+  ]
+})
