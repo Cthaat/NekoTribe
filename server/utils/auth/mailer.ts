095486bea51b0c2003775990e4beea86e08ed9f9
Edge <edge27452696803@gmail.com>
2025-07-09 10:48:48 +0800

    style(mailer.ts): 统一代码格式，添加缺失的分号

--- server/utils/auth/mailer.ts
+++ server/utils/auth/mailer.ts
@@ -1,14 +1,14 @@
-import nodemailer from 'nodemailer'
-import type { SentMessageInfo } from 'nodemailer'
-import type { EmailOptions } from '../type'
+import nodemailer from 'nodemailer';
+import type { SentMessageInfo } from 'nodemailer';
+import type { EmailOptions } from '../type';
 
-const runtimeConfig = useRuntimeConfig()
- 
+const runtimeConfig = useRuntimeConfig();
+
 // 定义一个用于发送邮件的类
 class Mailer {
   // 私有静态属性，用于存储 nodemailer 的 Transporter 实例
-  private static transporter: nodemailer.Transporter
- 
+  private static transporter: nodemailer.Transporter;
+
   // 私有静态方法，用于获取或创建 nodemailer 的 Transporter 实例
   private static getTransporter(): nodemailer.Transporter {
     if (!Mailer.transporter) {
@@ -20,21 +20,21 @@ class Mailer {
           user: runtimeConfig.smtpUser,
           pass: runtimeConfig.smtpPass
         }
-      })
+      });
     }
-    return Mailer.transporter
+    return Mailer.transporter;
   }
- 
+
   // 静态异步方法，用于发送邮件
   static async sendMail(options: EmailOptions): Promise<SentMessageInfo> {
-    const transporter = this.getTransporter()
+    const transporter = this.getTransporter();
     return transporter.sendMail({
       from: runtimeConfig.smtpUser,
       to: options.to,
       subject: options.subject,
       text: options.text,
       html: options.html
-    })
+    });
   }
 }
-export default Mailer
+export default Mailer;
