54becc6236274f2974c8fe41bb54ae3788afd566
Edge <edge27452696803@gmail.com>
2025-07-09 10:40:08 +0800

    style(auth.ts): 统一代码格式，添加缺失的分号

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,22 +1,22 @@
-import jwt from 'jsonwebtoken'
-import { getRequestURL } from 'h3'
+import jwt from 'jsonwebtoken';
+import { getRequestURL } from 'h3';
 
 const runtimeConfig = useRuntimeConfig();
 
 const WHITE_LIST = [
   '/api/v1/auth/'
   // 可以继续添加其他前缀
-]
+];
 
-export default defineEventHandler((event) => {
-  const url = getRequestURL(event).pathname
+export default defineEventHandler(event => {
+  const url = getRequestURL(event).pathname;
 
   // 判断是否命中白名单前缀
   if (WHITE_LIST.some(prefix => url.startsWith(prefix))) {
-    return
+    return;
   }
 
-  const token = getCookie(event, 'access_token')
+  const token = getCookie(event, 'access_token');
   if (!token) {
     throw createError({
       statusCode: 401,
@@ -27,11 +27,11 @@ export default defineEventHandler((event) => {
         code: 401,
         timestamp: new Date().toISOString()
       } as ErrorResponse
-    })
+    });
   }
   try {
-    const payload = jwt.verify(token, runtimeConfig.accessSecret)
-    event.context.auth = payload
+    const payload = jwt.verify(token, runtimeConfig.accessSecret);
+    event.context.auth = payload;
   } catch (err) {
     throw createError({
       statusCode: 401,
@@ -42,6 +42,6 @@ export default defineEventHandler((event) => {
         code: 401,
         timestamp: new Date().toISOString()
       } as ErrorResponse
-    })
+    });
   }
-})
+});
