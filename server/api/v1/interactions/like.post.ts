3393b8d040d85d2b91e75d035045007f1ff4ba8d
Edge <edge27452696803@gmail.com>
2025-07-11 01:13:49 +0800

    fix(like): 修复点赞操作中的默认行为逻辑

--- server/api/v1/interactions/like.post.ts
+++ server/api/v1/interactions/like.post.ts
@@ -26,7 +26,7 @@ export default defineEventHandler(async event => {
       {
         user_id: user.userId,
         target_tweet_id: body.tweetId,
-        action: body.likeType,
+        action: body.likeType === 'like' || 'unlike' ? body.likeType : 'like',
         p_result: {
           dir: oracledb.BIND_OUT,
           type: oracledb.STRING,
