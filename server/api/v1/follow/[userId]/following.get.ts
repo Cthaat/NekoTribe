5cb52a4a207f7b4afb3601342831b0d6b246b62c
Edge <edge27452696803@gmail.com>
2025-07-10 23:49:15 +0800

    feat(follow): 添加获取用户关注列表的接口实现

---
+++ server/api/v1/follow/[userId]/following.get.ts
@@ -0,0 +1,27 @@
+export default defineEventHandler(async event => {
+  // 获取当前登录用户信息
+  const user: Auth = event.context.auth as Auth;
+  // 获取 tweetId 路径参数
+  const userId: string = getRouterParam(event, 'userId') as string;
+
+  if (!userId) {
+    throw createError({
+      statusCode: 400,
+      message: '缺少用户ID',
+      data: {
+        success: false,
+        message: '缺少用户ID',
+        code: 400,
+        timestamp: new Date().toISOString()
+      } as ErrorResponse
+    });
+  }
+
+  const getOracleConnection = event.context.getOracleConnection;
+  const connection = await getOracleConnection();
+
+  try {
+  } finally {
+    await connection.close();
+  }
+});
