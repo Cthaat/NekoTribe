af2cae97087e347853500c9bf564e5bcd941f3e7
Edge <edge27452696803@gmail.com>
2025-07-13 00:11:02 +0800

    feat(auth): 将事件处理程序改为异步，新增数据库连接以更新用户最后访问时间

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -8,8 +8,11 @@ const WHITE_LIST = [
   // 可以继续添加其他前缀
 ];
 
-export default defineEventHandler(event => {
+export default defineEventHandler(async event => {
   const url = getRequestURL(event).pathname;
+
+  const getOracleConnection = event.context.getOracleConnection;
+  const connection = await getOracleConnection();
 
   // 判断是否命中白名单前缀
   if (WHITE_LIST.some(prefix => url.startsWith(prefix))) {
@@ -32,6 +35,26 @@ export default defineEventHandler(event => {
   try {
     const payload = jwt.verify(token, runtimeConfig.accessSecret);
     event.context.auth = payload;
+
+    const auth: Auth = payload as Auth;
+
+    // 更新用户的最后访问时间
+    const updateSql = `
+    UPDATE n_user_sessions
+    SET
+    last_accessed_at = SYSDATE
+    WHERE user_id = :userId
+    AND DBMS_LOB.COMPARE(access_token, :accessToken) = 0
+    `;
+
+    await connection.execute(
+      updateSql,
+      {
+        userId: auth.userId,
+        accessToken: token
+      },
+      { autoCommit: true }
+    );
   } catch (err) {
     throw createError({
       statusCode: 401,
