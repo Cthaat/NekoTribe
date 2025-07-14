d2b2b6c73e2977e15c8c9b5423b1ac49e886496c
Edge <edge27452696803@gmail.com>
2025-07-15 02:48:19 +0800

    style(search.get): 优化代码格式，提升可读性

--- server/api/v1/users/search.get.ts
+++ server/api/v1/users/search.get.ts
@@ -1,13 +1,16 @@
 export default defineEventHandler(async event => {
   // 获取 query 参数
-  const query: SearchUsersPayload = getQuery(event) as SearchUsersPayload;
+  const query: SearchUsersPayload = getQuery(
+    event
+  ) as SearchUsersPayload;
 
   // 提取参数
   const q: string = (query.q || '') as string;
   const page: number = (query.page || 1) as number;
   const pageSize: number = (query.pageSize || 10) as number;
 
-  const getOracleConnection = event.context.getOracleConnection;
+  const getOracleConnection =
+    event.context.getOracleConnection;
   const connection = await getOracleConnection();
 
   try {
