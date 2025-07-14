e923735a7e97249e0598bf7a0eb8349c731cffbf
Edge <edge27452696803@gmail.com>
2025-07-15 02:48:33 +0800

    style(suggestions.get): 优化代码格式，提升可读性

--- server/api/v1/users/suggestions.get.ts
+++ server/api/v1/users/suggestions.get.ts
@@ -7,7 +7,8 @@ export default defineEventHandler(async event => {
   // 提取参数
   const limit: number = (query.limit || 10) as number;
 
-  const getOracleConnection = event.context.getOracleConnection;
+  const getOracleConnection =
+    event.context.getOracleConnection;
   const connection = await getOracleConnection();
 
   try {
@@ -30,9 +31,11 @@ export default defineEventHandler(async event => {
       success: true,
       message: 'Search users successfully',
       data: {
-        users: result.rows.map((row: ReturnSuggestionUserId[]) => ({
-          userId: row[0]
-        }))
+        users: result.rows.map(
+          (row: ReturnSuggestionUserId[]) => ({
+            userId: row[0]
+          })
+        )
       },
       code: 200,
       timestamp: new Date().toISOString()
