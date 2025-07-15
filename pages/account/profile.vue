cd32cb914ec2e2775d2b551e449cff9675d47c53
Edge <edge27452696803@gmail.com>
2025-07-15 15:51:40 +0800

    feat(profile.vue): 添加个人资料表单组件并优化页面布局

--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { Separator } from '@/components/ui/separator';
+import ProfileForm from '@/components/ProfileForm.vue';
+</script>
+
 <template>
   <Card>
-    <CardContent class="p-6"> 123 </CardContent>
+    <CardContent>
+      <div class="hidden space-y-6 p-10 pb-16 md:block">
+        <div class="space-y-0.5">
+          <h2 class="text-2xl font-bold tracking-tight">
+            Profile
+          </h2>
+          <p class="text-muted-foreground">
+            This is how others will see you on the site.
+          </p>
+        </div>
+        <Separator class="my-6" />
+        <ProfileForm />
+      </div>
+    </CardContent>
   </Card>
 </template>
