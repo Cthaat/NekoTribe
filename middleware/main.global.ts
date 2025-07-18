7bba2fbe38d35fea278e1472360fae808a69cc84
Edge <edge27452696803@gmail.com>
2025-07-18 22:29:09 +0800

    fix(middleware): 移除调试日志以清理代码

--- middleware/main.global.ts
+++ middleware/main.global.ts
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware(to => {
   const preferenceStore = usePreferenceStore();
   const localePath = useLocalePath();
   const userId = preferenceStore.preferences.user.userId;
-  console.log('userId', userId);
   const homePath = localePath(`/tweet/home/${userId}`);
   if (to.path === '/') {
     return navigateTo(homePath, { replace: true });
