a22f4e13844475785f608485237f5818de4f36e5
Edge <edge27452696803@gmail.com>
2026-04-27 16:25:42 +0800

    refactor: 优化代码格式，提高可读性

--- server/api/v2/users/me/mutes/[target_id].post.ts
+++ server/api/v2/users/me/mutes/[target_id].post.ts
@@ -1,6 +1,13 @@
 import { v2MuteUser } from '~/server/services/v2/users';
-import { defineV2Handler, v2RouterNumber } from '~/server/utils/v2';
+import {
+  defineV2Handler,
+  v2RouterNumber
+} from '~/server/utils/v2';
 
 export default defineV2Handler((event, connection) =>
-  v2MuteUser(event, connection, v2RouterNumber(event, 'target_id'))
+  v2MuteUser(
+    event,
+    connection,
+    v2RouterNumber(event, 'target_id')
+  )
 );
