0cd4e377f20374f63e0236aaaa3ecf336325991e
Edge <edge27452696803@gmail.com>
2026-04-27 16:32:30 +0800

    refactor: 优化代码格式，提高可读性

--- server/api/v2/posts/[id]/replies.get.ts
+++ server/api/v2/posts/[id]/replies.get.ts
@@ -1,5 +1,8 @@
 import { v2ListPosts } from '~/server/services/v2/posts';
-import { defineV2Handler, v2RouterNumber } from '~/server/utils/v2';
+import {
+  defineV2Handler,
+  v2RouterNumber
+} from '~/server/utils/v2';
 
 export default defineV2Handler((event, connection) =>
   v2ListPosts(event, connection, {
