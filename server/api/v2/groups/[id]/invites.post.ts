924df7f2335914db2bcc4743ef134f8588064f77
Edge <edge27452696803@gmail.com>
2026-04-27 19:46:14 +0800

    refactor: 优化代码格式，提高可读性

--- server/api/v2/groups/[id]/invites.post.ts
+++ server/api/v2/groups/[id]/invites.post.ts
@@ -1,6 +1,13 @@
 import { v2CreateGroupInvite } from '~/server/services/v2/groups';
-import { defineV2Handler, v2RouterNumber } from '~/server/utils/v2';
+import {
+  defineV2Handler,
+  v2RouterNumber
+} from '~/server/utils/v2';
 
 export default defineV2Handler((event, connection) =>
-  v2CreateGroupInvite(event, connection, v2RouterNumber(event, 'id'))
+  v2CreateGroupInvite(
+    event,
+    connection,
+    v2RouterNumber(event, 'id')
+  )
 );
