23aed42277659ab253877134e85d688c4f8801cf
Edge <edge27452696803@gmail.com>
2026-04-27 19:41:52 +0800

    refactor: 优化代码格式，提高可读性

--- server/api/v2/groups/[id]/posts/[post_id]/comments/index.get.ts
+++ server/api/v2/groups/[id]/posts/[post_id]/comments/index.get.ts
@@ -1,6 +1,13 @@
 import { v2GroupComments } from '~/server/services/v2/groups';
-import { defineV2Handler, v2RouterNumber } from '~/server/utils/v2';
+import {
+  defineV2Handler,
+  v2RouterNumber
+} from '~/server/utils/v2';
 
 export default defineV2Handler((event, connection) =>
-  v2GroupComments(event, connection, v2RouterNumber(event, 'post_id'))
+  v2GroupComments(
+    event,
+    connection,
+    v2RouterNumber(event, 'post_id')
+  )
 );
