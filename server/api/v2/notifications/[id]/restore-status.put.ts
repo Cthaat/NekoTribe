a12f8bd3d28bd623d13d4a40fde074f74d445d55
Edge <edge27452696803@gmail.com>
2026-04-27 17:01:09 +0800

    refactor: 优化导入格式，提高可读性

--- server/api/v2/notifications/[id]/restore-status.put.ts
+++ server/api/v2/notifications/[id]/restore-status.put.ts
@@ -1,5 +1,8 @@
 import { v2RestoreNotification } from '~/server/services/v2/notifications';
-import { defineV2Handler, v2RouterNumber } from '~/server/utils/v2';
+import {
+  defineV2Handler,
+  v2RouterNumber
+} from '~/server/utils/v2';
 
 export default defineV2Handler((event, connection) =>
   v2RestoreNotification(
